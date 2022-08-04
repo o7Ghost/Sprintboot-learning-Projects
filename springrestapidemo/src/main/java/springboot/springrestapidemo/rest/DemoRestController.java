e91f20de8d1e385275056459b11ee4db2e5e86b1
Joseph <qjlu@ucsd.edu>
2022-08-04 15:33:13 -0700

    added error handling for student id resquest that's not a number or none exists in database

--- springrestapidemo/src/main/java/springboot/springrestapidemo/rest/DemoRestController.java
+++ springrestapidemo/src/main/java/springboot/springrestapidemo/rest/DemoRestController.java
@@ -1,4 +1,4 @@
-package springboot.springrestapidemo;
+package springboot.springrestapidemo.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
---
+++ springrestapidemo/src/main/java/springboot/springrestapidemo/rest/StudentNotFoundException.java
@@ -0,0 +1,17 @@
+package springboot.springrestapidemo.rest;
+
+public class StudentNotFoundException extends RuntimeException {
+
+    public StudentNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    public StudentNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public StudentNotFoundException(String message) {
+        super(message);
+    }
+
+}
